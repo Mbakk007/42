189c53387778a4b13eab3563b42813641d507c7e
Mbakk007 <moutazbakk@gmail.com>
2026-02-09 12:14:39 +0100

    rm

--- Python modules/Python Module 00/ex4/ft_water_reminder.py
+++
@@ -1,6 +0,0 @@
-def ft_water_reminder():
-    days = int(input("Days since last watering: "))
-    if days <= 2:
-        print("Plants are fine")
-    else:
-        print("Water the plants!")
--- Python modules/Python Module 00/ex6/ft_garden_summary.py
+++
@@ -1,6 +0,0 @@
-def ft_garden_summary():
-    name = input("Enter garden name: ")
-    Plants = int(input("Enter number of plants: "))
-    print(f"Garden: {name}")
-    print(f"Plants: {Plants}")
-    print("Status: Growing well!")
